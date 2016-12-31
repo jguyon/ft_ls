31089c8f592ab30296085e2a1f9e103ad30c7ba4
Jerome Guyon <jguyon@student.42.fr>
2016-12-31 16:18:25 +0100

    Optimize ls_join_path

--- srcs/ls_join_path.c
+++ srcs/ls_join_path.c
@@ -6,7 +6,7 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/11 21:49:24 by jguyon            #+#    #+#             */
-/*   Updated: 2016/12/11 22:11:31 by jguyon           ###   ########.fr       */
+/*   Updated: 2016/12/31 16:17:12 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ char	*ls_join_path(const char *dirname, const char *filename)
 		return (ft_strjoin(dirname, filename));
 	else if ((path = ft_strnew(len + 1 + ft_strlen(filename))))
 	{
-		ft_strcat(path, dirname);
-		ft_strcat(path, "/");
-		ft_strcat(path, filename);
+		ft_strcpy(path, dirname);
+		path[len] = '/';
+		ft_strcpy(path + len + 1, filename);
 		return (path);
 	}
 	return (NULL);
