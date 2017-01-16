1869a13ffd30026348f7eb89c2da78c5ad165a06
Jérôme Guyon <jeromep.guyon@gmail.com>
2017-01-16 13:25:50 +0100

    Fix norm

--- srcs/ls_print_time.c
+++ srcs/ls_print_time.c
@@ -6,7 +6,7 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/16 12:34:55 by jguyon            #+#    #+#             */
-/*   Updated: 2017/01/16 12:52:59 by jguyon           ###   ########.fr       */
+/*   Updated: 2017/01/16 13:25:08 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ void	ls_print_time(time_t t)
 	str = ctime(&t);
 	now = time(NULL);
 	ft_fprintf(FT_STDOUT, " %.6s %.5s", str + 4,
-			   (t + LS_SIX_MONTHS <= now || now + LS_SIX_MONTHS <= t)
-			   ? str + 19 : str + 11);
+		(t + LS_SIX_MONTHS <= now || now + LS_SIX_MONTHS <= t)
+			? str + 19 : str + 11);
 }
