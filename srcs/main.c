446c59aeca6b1c900f5ad0ac9417e3c43c9cea29
Jérôme Guyon <jeromep.guyon@gmail.com>
2017-01-16 20:31:46 +0100

    Clean up and fix norm of main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/10 10:53:25 by jguyon            #+#    #+#             */
-/*   Updated: 2017/01/16 20:08:15 by jguyon           ###   ########.fr       */
+/*   Updated: 2017/01/16 20:31:17 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,43 +27,45 @@ static void	clear_caches(void)
 	ls_cache_clear(&g_ls_groups);
 }
 
+static void	print_files(t_args *args)
+{
+	ls_sort_files(args->flags, &(args->dirs));
+	if (!ft_dlst_empty(&(args->files)))
+	{
+		ls_sort_files(args->flags, &(args->files));
+		ls_print_files(args->flags, &(args->files),
+						&(args->dinfo), &(args->dirs));
+	}
+}
+
+static int	print_next(int is_single, t_args *args)
+{
+	t_file	*dir;
+
+	if (!(dir = ls_pop_next(args->flags, &(args->dirs))))
+		return (0);
+	ls_list_files(args->flags, dir, &(args->dinfo), &(args->files));
+	ls_sort_files(args->flags, &(args->files));
+	ls_print_dirinfo(is_single, args->flags, dir, &(args->dinfo));
+	ls_print_files(args->flags, &(args->files), &(args->dinfo), &(args->dirs));
+	ls_destroy_file(dir);
+	return (1);
+}
+
 int			main(int argc, char **argv)
 {
-	t_args			args;
-	t_file			*dir;
+	t_args	args;
 
 	ls_setprogname(argv[0]);
 	ls_atexit(&close_streams);
 	ls_atexit(&clear_caches);
 	if (!(ls_parse_args(argc, argv, &args)))
 		ls_exit(LS_EXIT_STATUS);
-	if (ft_dlst_singular(&(args.dirs))
-		&& ft_dlst_empty(&(args.files))
-		&& (dir = ls_pop_next(args.flags, &(args.dirs))))
-	{
-		ls_list_files(args.flags, dir, &(args.dinfo), &(args.files));
-		ls_sort_files(args.flags, &(args.files));
-		ls_print_dirinfo(1, args.flags, dir, &(args.dinfo));
-		ls_print_files(args.flags, &(args.files), &(args.dinfo), &(args.dirs));
-		ls_destroy_file(dir);
-	}
+	if (ft_dlst_singular(&(args.dirs)) && ft_dlst_empty(&(args.files)))
+		print_next(1, &args);
 	else
-	{
-		ls_sort_files(args.flags, &(args.dirs));
-		if (!ft_dlst_empty(&(args.files)))
-		{
-			ls_sort_files(args.flags, &(args.files));
-			ls_print_files(args.flags, &(args.files),
-							&(args.dinfo), &(args.dirs));
-		}
-	}
-	while ((dir = ls_pop_next(args.flags, &(args.dirs))))
-	{
-		ls_list_files(args.flags, dir, &(args.dinfo), &(args.files));
-		ls_sort_files(args.flags, &(args.files));
-		ls_print_dirinfo(0, args.flags, dir, &(args.dinfo));
-		ls_print_files(args.flags, &(args.files), &(args.dinfo), &(args.dirs));
-		ls_destroy_file(dir);
-	}
+		print_files(&args);
+	while (print_next(0, &args))
+		;
 	return (ls_cleanup(LS_EXIT_STATUS));
 }
