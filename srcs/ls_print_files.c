9f25ea5407248a6fcd416b876f49d0b3c90590e0
Jérôme Guyon <jeromep.guyon@gmail.com>
2017-01-28 21:32:24 +0100

    Fix total in long format

--- srcs/ls_print_files.c
+++ srcs/ls_print_files.c
@@ -6,7 +6,7 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/25 16:39:09 by jguyon            #+#    #+#             */
-/*   Updated: 2017/01/28 21:14:01 by jguyon           ###   ########.fr       */
+/*   Updated: 2017/01/28 21:26:17 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,10 @@
 #include "ls_callbacks.h"
 #include "ft_printf.h"
 
-static int	g_is_long = 0;
-
-static int	print_single(void *dirinfo, t_file *file)
-{
-	(void)file;
-	if (g_is_long)
-		ls_print_total(dirinfo);
-	if (ft_ferror(FT_STDOUT))
-		return (-1);
-	return (0);
-}
-
 static int	print_first(void *dirinfo, t_file *file)
 {
+	(void)dirinfo;
 	ft_fprintf(FT_STDOUT, "%s:\n", file->path);
-	if (g_is_long)
-		ls_print_total(dirinfo);
 	if (ft_ferror(FT_STDOUT))
 		return (-1);
 	return (0);
@@ -38,9 +25,8 @@ static int	print_first(void *dirinfo, t_file *file)
 
 static int	print_dir(void *dirinfo, t_file *file)
 {
+	(void)dirinfo;
 	ft_fprintf(FT_STDOUT, "\n%s:\n", file->path);
-	if (g_is_long)
-		ls_print_total(dirinfo);
 	if (ft_ferror(FT_STDOUT))
 		return (-1);
 	return (0);
@@ -51,12 +37,17 @@ int			ls_print_files(t_flags *flags, t_flist *flist)
 	t_file			*dir;
 	t_flist_print	*print;
 
-	g_is_long = (flags->format == LS_FORMAT_LONG);
-	print = flags->singlearg ? &print_single : &print_first;
+	print = flags->singlearg ? NULL : &print_first;
 	if (ls_flist_print(flist) && (dir = ls_flist_next(flist, &print_dir)))
+	{
+		if (flags->format == LS_FORMAT_LONG)
+			ls_print_total(flist->dirinfo);
 		ls_flist_print(flist);
+	}
 	else if ((dir = ls_flist_next(flist, print)))
 	{
+		if (flags->format == LS_FORMAT_LONG)
+			ls_print_total(flist->dirinfo);
 		ls_flist_print(flist);
 		ls_file_del(&dir);
 	}
